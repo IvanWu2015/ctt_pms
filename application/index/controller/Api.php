3cd34d98b90f55ff12a3d0e9943448441ccb9a93
oschina_ivanwu <wuxin08@vip.qq.com>
2017-02-27 11:40:40 +0800

    增加API接口文件，用于输出AJAX请求数据

---
+++ application/index/controller/Api.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * API集中处理
+ */
+
+namespace app\index\controller;
+
+use \think\Db;
+
+class api extends Common {
+
+    //输出日历需要的工时
+    public function getworkdata() {
+        //接收年、月、日
+        $year = input('year', 0, 'intval');
+        $month = input('month', 0, 'intval');
+        $day = input('day', 0, 'intval');
+        /**
+         * todo:此处需要根据年月日数据返回对应的任务信息或是工时信息
+         * 如果没有指定日则默认返回当月数据
+         */
+        return ;
+    }
+
+}
