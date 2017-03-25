0222618346e8ba8bb3c97fbcdf6c5806676bfbc4
oschina_ivanwu <wuxin08@vip.qq.com>
2017-03-26 01:33:18 +0800

    首页入口文件修改，作自动跳转public处理 增加COMPOSER更新后未加入的文件

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
-
-// [ 应用入口文件 ]
-// 定义应用目录
-define('APP_PATH', __DIR__ . './application/');
-
-// 加载框架引导文件
-require __DIR__ . './thinkphp/start.php';
+/*实现至TP5入口的跳转
+不过实际使用建议直接网站网站目录设置为public
+*/
+header("Location: http://". $_SERVER['SERVER_NAME'] . rtrim(dirname(rtrim($_SERVER['SCRIPT_NAME'], '/')), '/').'/public' );exit();
+?>
