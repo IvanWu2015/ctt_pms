b000742175d65a22e41381e830c7a100674d5e5b
ming2017 <2273659300@qq.com>
2017-08-03 16:34:31 +0800

    个人中心待完成的任务调整、顶部导航当前状态、添加任务页添加该项目标题头部、任务详情右上角添加任务按钮

---
+++ public/source/js/limit.js
@@ -0,0 +1,15 @@
+﻿jQuery.fn.limit=function(){
+    var self = jQuery("span[limit]"); 
+    self.each(function(){ 
+        var objString = jQuery(this).text(); 
+        var objLength = jQuery(this).text().length; 
+        var num = jQuery(this).attr("limit"); 
+        if(objLength > num){ 
+            jQuery(this).attr("title",objString); 
+            objString = jQuery(this).html(objString.substring(0,num)); 
+        } 
+    }) 
+} 
+jQuery(function(){ 
+    jQuery(document.body).limit(); 
+})
