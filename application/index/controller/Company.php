6687bdcdbf06498bc5f9f2a380b4afffdea06cdf
oschina_ivanwu <wuxin08@vip.qq.com>
2018-02-27 16:52:19 +0800

    公司及联系人添加列表处理

--- application/index/controller/Company.php
+++ application/index/controller/Company.php
@@ -1,53 +1,68 @@
 <?php
+
 //公司管理
+
 namespace app\index\controller;
+
 use \traits\controller\Jump;
 use \think\Db;
+
 load_trait('controller/Jump');  // 引入traits\controller\Jump
 
 class Company extends Common {
 
     //公司列表
     public function lists() {
-        $dept = db('Dept');
-        // $pro_id = intval($_GET['pro_id']);
-        $pro_id = input('get.pro_id', '0', 'intval');
-        $dept_list = $dept->select();
-        $team = db('Team');
-        $team_user = $team->where(['project' => $pro_id])->select();
-
-
-        $this->assign('dept_list', $dept_list);
+        $username = input('get.name', '', 'addslashes');
+        if (!empty($username)) {
+            $map['name'] = array('eq', $username);
+        }
+        $company_list = DB::name('company')
+                ->where($map)
+                ->paginate(10);
+        $this->assign('company_list', $company_list);
         return $this->fetch($this->templatePath);
     }
-    
+
     //添加公司
     public function add() {
-        $dept = db('Dept');
-        $user = db('User');
-        $user_list = $user->select();
-        //$dept_id = intval($_GET['dept_id']);
-        $dept_id = input('get.dept_id', '0', 'intval');
+        $company = db('Company');
+        //$company_id = intval($_GET['dept_id']);
+        $company_id = input('company_id', '0', 'intval');
         if (IS_POST) {
-            $dept_data = array(
-                'name' => input('post.name', '', 'strip_tags'),
-                'manager' => input('post.manager', '', 'strip_tags'),
-                'function' => input('post.function', '', 'strip_tags'),
+            $company_data = array(
+                'name' => input('name', '', 'strip_tags'), //'公司名称',
+                'corporation' => input('corporation', '', 'strip_tags'), //'法人',
+                'type' => input('type', '', 'strip_tags'), // '1国企 2私营 3合资 4外资',
+                'founding_time' => input('founding_time', '', 'strip_tags'), // '成立时间',
+                'employee_numbers' => input('employee_numbers', '', 'strip_tags'), //1 10人以内  2 10-50  3 50-100 4 100-500 6 500-1000 7 1000以上',
+                'resister_address' => input('resister_address', '', 'strip_tags'), //'注册地址',
+                'work_address' => input('post.work_address', '', 'strip_tags'), //'办公地址',
+                'contact_name' => input('post.contact_name', '', 'strip_tags'), // '联系人',
+                'contact_number' => input('post.contact_number', '', 'strip_tags'), // '联系电话',
+                'fax_number' => input('post.fax_number', '', 'strip_tags'), // '传真电话',
+                'tax_number' => input('post.name', '', 'strip_tags'), // '税号',
+                'bank_deposit' => input('post.name', '', 'strip_tags'), //'开户行',
+                'bank_account' => input('post.name', '', 'strip_tags'), // '银行账号',
             );
 
-            if ($dept_id > 0) {
-                $dept->where(['id' => $dept_id])->save($dept_data);
+            if ($company_id > 0) {
+                $company_data['last_uid'] = $_G['uid']; //'最后修改人',
+                $company_data['last_time'] = date("Y-m-d H:i:s"); //'最后修改时间',
+                $company->where(['company_id' => $company_id])->save($company_data);
+                $this->success("成功添加", 'Product/detail?id=' . $product_detail['id']);
             } else {
-                $dept->data($dept_data)->insert();
+                $company_data['add_uid'] = $_G['uid']; //'最后修改人',
+                $company_data['add_time'] = date("Y-m-d H:i:s"); //'最后修改时间',
+                $company->data($company_data)->insert();
             }
         }
-
-        $this->assign('user_list', $user_list);
         return $this->fetch($this->templatePath);
     }
-    
+
     //公司详情页
     public function detail() {
         return $this->fetch($this->templatePath);
     }
+
 }
