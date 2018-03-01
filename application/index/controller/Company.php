f897c2208941a6fdd5b4efc717c12a4459e75bbc
oschina_ivanwu <wuxin08@vip.qq.com>
2018-03-01 17:18:45 +0800

    增加getCommonConfigList用于获取分组设置 公司类似与规模列表改由后台设置 完善公司、联系人的搜索、添加跳转等内容

--- application/index/controller/Company.php
+++ application/index/controller/Company.php
@@ -6,6 +6,7 @@ namespace app\index\controller;
 
 use \traits\controller\Jump;
 use \think\Db;
+use Page;
 
 load_trait('controller/Jump');  // 引入traits\controller\Jump
 
@@ -19,14 +20,22 @@ class Company extends Common {
 
     //公司列表
     public function lists() {
-        $username = input('get.name', '', 'addslashes');
-        if (!empty($username)) {
-            $map['name'] = array('eq', $username);
+        $name = input('name', '', 'addslashes');
+        $contact_name = input('contact_name', '', 'addslashes');
+        if (!empty($name)) {
+            $map['name'] = array('like', "%{$name}%");
+        }
+        if (!empty($contact_name)) {
+            $map['contact_name'] = array('like', "%{$contact_name}%");
         }
         $company_list = DB::name('company')
                 ->where($map)
                 ->paginate(10);
+        $page = $company_list->render(); // 分页显示输出
         $this->assign('company_list', $company_list);
+        $this->assign('page', $page);
+        $this->assign('name', $name); //搜索公司名关键字
+        $this->assign('contact_name', $contact_name); //搜索联系人关键字
         return $this->fetch($this->templatePath);
     }
 
@@ -35,9 +44,12 @@ class Company extends Common {
         $company = db('Company');
         //$company_id = intval($this->_GET['dept_id']);
         $company_id = input('id', '0', 'intval');
-        if($company_id > 0) {
-             $companyDetail = $company->where("company_id = {$company_id}")->find();
+        if ($company_id > 0) {
+            $companyDetail = $company->where("company_id = {$company_id}")->find();
         }
+        
+        $companyTypeList = getCommonConfigList('company_type');//公司类型列表
+        $employeeTypeList = getCommonConfigList('employee_type');//公司规模列表
         if (request()->isPost()) {
             $companyData = array(
                 'name' => input('name', '', 'strip_tags'), //'公司名称',
@@ -50,24 +62,26 @@ class Company extends Common {
                 'contact_name' => input('post.contact_name', '', 'strip_tags'), // '联系人',
                 'contact_number' => input('post.contact_number', '', 'strip_tags'), // '联系电话',
                 'fax_number' => input('post.fax_number', '', 'strip_tags'), // '传真电话',
-                'tax_number' => input('post.name', '', 'strip_tags'), // '税号',
-                'bank_deposit' => input('post.name', '', 'strip_tags'), //'开户行',
-                'bank_account' => input('post.name', '', 'strip_tags'), // '银行账号',
+                'tax_number' => input('post.tax_number', '', 'strip_tags'), // '税号',
+                'bank_deposit' => input('post.bank_deposit', '', 'strip_tags'), //'开户行',
+                'bank_account' => input('post.bank_account', '', 'strip_tags'), // '银行账号',
             );
 
             if ($company_id > 0) {
                 $companyData['last_uid'] = $this->_G['uid']; //'最后修改人',
                 $companyData['last_time'] = date("Y-m-d H:i:s"); //'最后修改时间',
-                $company->where(['company_id' => $company_id])->save($companyData);
-                $this->success("修改成功", 'Company/detail?id=' . $companyData['company_id']);
+                $company->where(['company_id' => $company_id])->update($companyData);
+                $this->success("修改成功", 'Company/lists');
             } else {
                 $companyData['add_uid'] = $this->_G['uid']; //'最后修改人',
                 $companyData['add_time'] = date("Y-m-d H:i:s"); //'最后修改时间',
                 $company->data($companyData)->insert();
-                $this->success("成功添加", 'Company/detail?id=' . $companyData['company_id']);
+                $this->success("成功添加", 'Company/lists');
             }
         }
         $this->assign('companyDetail', $companyDetail);
+        $this->assign('companyTypeList', $companyTypeList);
+        $this->assign('employeeTypeList', $employeeTypeList);
         return $this->fetch($this->templatePath);
     }
 
@@ -87,8 +101,7 @@ class Company extends Common {
         $map = ['company_id' => $company_id];
         $contacts_list = DB::name('Contact')
                 ->where($map)
-                ->paginate(10);
-        
+                ->select();
         $this->assign('contacts_list', $contacts_list);
         $this->assign('navtitle', $navtitle);
         $this->assign('companyDetail', $companyDetail);
