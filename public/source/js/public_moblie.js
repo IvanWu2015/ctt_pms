917ab02973d1d85f9daeb3af8187a374699f2b7e
ming2017 <2273659300@qq.com>
2018-03-01 10:36:32 +0800

    手机：联系人列表完善

--- public/source/js/public_moblie.js
+++ public/source/js/public_moblie.js
@@ -1,13 +1,19 @@
 ﻿function navopen(){ //展开导航
     jQuery('#moblie_nav').fadeIn();
-    jQuery('#nav_ul').css({transform: 'translateX(-100%)'});
+    jQuery('#nav_ul').css({right: '0'});
+    //jQuery('#nav_ul').css({transform: 'translateX(-100%)'});
     jQuery('#wrap').css({transform: 'translateX(-50%)'});
+    jQuery('#navwp').css({transform: 'translateX(-50%)'});
+    jQuery('.footer_wp').css({transform: 'translateX(-50%)'});
 }
 
 function navclose(){ //关闭导航
     jQuery('#moblie_nav').fadeOut();
-    jQuery('#nav_ul').css({transform: 'translateX(0%)'});
+    //jQuery('#nav_ul').css({transform: 'translateX(0%)'});
+    jQuery('#nav_ul').css({right: '-50%'});
     jQuery('#wrap').css({transform: 'translateX(0)'});
+    jQuery('#navwp').css({transform: 'translateX(0)'});
+    jQuery('.footer_wp').css({transform: 'translateX(0)'});
 }
 
 function searchopen(name){  //搜索打开、关闭
