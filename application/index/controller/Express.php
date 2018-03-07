e473d30649524447dcb6aa3099da4d2d33436bbd
oschina_ivanwu <wuxin08@vip.qq.com>
2018-03-07 12:39:38 +0800

    增加快递单搜索功能 修复详情页读取错误

--- application/index/controller/Express.php
+++ application/index/controller/Express.php
@@ -20,24 +20,31 @@ class Express extends Common {
 
     //快递列表
     public function lists() {
-        $name = input('name', '', 'addslashes');
-        $contact_name = input('contact_name', '', 'addslashes');
+        $name = input('name', '', 'addslashes'); //关键字
+        $start_time = input('start_time', '', 'addslashes'); //开始时间
+        $end_time = input('end_time', '', 'addslashes'); //结束时间
         if (!empty($name)) {
-            $map['name'] = array('like', "%{$name}%");
+            $map['express_name|from_name|from_tel|from_address|to_name|to_tel|to_address|number'] = array('like', "%{$name}%");
         }
-        if (!empty($contact_name)) {
-            $map['contact_name'] = array('like', "%{$contact_name}%");
+        if (!empty($start_time)) {
+            $map['add_time'] = array('gt', "$start_time");
+        }
+        if (!empty($end_time)) {
+            $map['add_time'] = array('lt', "$end_time");
         }
         $express_list = DB::name('express')
                 ->where($map)
-                ->paginate(10);
+                ->order('id DESC')
+                ->paginate(50);
         $page = $express_list->render(); // 分页显示输出
         $navtitle = '快递列表';
         $this->assign('navtitle', $navtitle);
         $this->assign('express_list', $express_list);
         $this->assign('page', $page);
         $this->assign('name', $name); //搜索快递名关键字
-        $this->assign('contact_name', $contact_name); //搜索联系人关键字
+        $this->assign('number', $number); //搜索联系人关键字
+        $this->assign('start_time', $start_time); //搜索快递名关键字
+        $this->assign('end_time', $end_time); //搜索联系人关键字
         return $this->fetch($this->templatePath);
     }
 
@@ -48,7 +55,7 @@ class Express extends Common {
         $express_id = input('id', '0', 'intval');
         if ($express_id > 0) {
             $navtitle = '编辑快递';
-            $expressDetail = $express->where("express_id = {$express_id}")->find();
+            $expressDetail = $express->where("id = {$express_id}")->find();
         } else {
             $navtitle = '添加快递';
         }
@@ -57,31 +64,27 @@ class Express extends Common {
         $employeeTypeList = getCommonConfigList('employee_type'); //快递规模列表
         if (request()->isPost()) {
             $expressData = array(
-                'name' => input('name', '', 'strip_tags'), //'快递名称',
-                'corporation' => input('corporation', '', 'strip_tags'), //'法人',
-                'type' => input('type', '', 'strip_tags'), // '1国企 2私营 3合资 4外资',
-                'founding_time' => input('founding_time', '', 'strip_tags'), // '成立时间',
-                'employee_numbers' => input('employee_numbers', '', 'strip_tags'), //1 10人以内  2 10-50  3 50-100 4 100-500 6 500-1000 7 1000以上',
-                'resister_address' => input('resister_address', '', 'strip_tags'), //'注册地址',
-                'work_address' => input('post.work_address', '', 'strip_tags'), //'办公地址',
-                'contact_name' => input('post.contact_name', '', 'strip_tags'), // '联系人',
-                'contact_number' => input('post.contact_number', '', 'strip_tags'), // '联系电话',
-                'fax_number' => input('post.fax_number', '', 'strip_tags'), // '传真电话',
-                'tax_number' => input('post.tax_number', '', 'strip_tags'), // '税号',
-                'bank_deposit' => input('post.bank_deposit', '', 'strip_tags'), //'开户行',
-                'bank_account' => input('post.bank_account', '', 'strip_tags'), // '银行账号',
-                'scope' => input('scope', 0, 'addslashes'), //'业务范围',
-                'remarks' => input('remarks', 0, 'addslashes'), //'备注信息',
+                'express_name' => input('express_name', '', 'strip_tags'), //'快递公司名称',
+                'express_number' => input('express_number', '', 'strip_tags'), //'快递单号',
+                'express_desc' => input('express_desc', '', 'strip_tags'), // '物品说明',
+                'from_name' => input('from_name', '', 'strip_tags'), // '发件人名称',
+                'to_name' => input('to_name', '', 'strip_tags'), //发件人名称
+                'to_tel' => input('to_tel', '', 'strip_tags'), //'发件人电话',
+                'company_id' => input('company_id', '', 'strip_tags'), //'公司ID',
+                'contact_id' => input('contact_id', '', 'strip_tags'), // '联系人ID',
+                'payment' => input('payment', '', 'strip_tags'), // '付款方式 1寄付 2到付',
+                'price' => input('price', '', 'strip_tags'), // '运费',
+                'weight' => input('weight', '', 'strip_tags'), // '重量 单位KG',
+                'remarks' => input('remarks', '', 'strip_tags'), //'其它说明',
+                'add_uid' => input('add_uid', '', 'strip_tags'), // '添加人uid',
+                'add_name' => input('add_name', 0, 'addslashes'), //'添加人名称',
             );
 
             if ($express_id > 0) {
-                $expressData['last_uid'] = $this->_G['uid']; //'最后修改人',
-                $expressData['last_time'] = date("Y-m-d H:i:s"); //'最后修改时间',
-                $express->where(['express_id' => $express_id])->update($expressData);
+                $express->where(['id' => $express_id])->update($expressData);
                 $this->success("修改成功", 'Express/lists');
             } else {
-                $expressData['add_uid'] = $this->_G['uid']; //'最后修改人',
-                $expressData['add_time'] = date("Y-m-d H:i:s"); //'最后修改时间',
+                $expressData['add_time'] = date("Y-m-d H:i:s"); //添加时间',
                 $express->insert($expressData);
                 $this->success("成功添加", 'Express/lists');
             }
@@ -95,10 +98,10 @@ class Express extends Common {
 
     //快递详情页
     public function detail() {
-        $express_id = input('get.id', '0', 'intval');
+        $id = input('get.id', '0', 'intval');
         $express = db('express');
-        if ($express_id > 0) {
-            $expressDetail = $express->where("express_id = {$express_id}")->find();
+        if ($id > 0) {
+            $expressDetail = $express->where("id = {$id}")->find();
         } else {
             $this->error('请输入ID');
         }
@@ -106,7 +109,7 @@ class Express extends Common {
             $this->error('不存在该数据');
         }
         $navtitle = '快递详情页';
-        $map = ['express_id' => $express_id];
+        $map = ['id' => $id];
         $contacts_list = DB::name('Contact')
                 ->where($map)
                 ->select();
